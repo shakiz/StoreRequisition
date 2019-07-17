f9422fefb45dce30710bbe535f5312ccb11cb97c
shakiz <activities335@gmail.com>
2019-07-17 16:30:52 +0600

    Modified the view items layout and details layout.

--- app/src/main/java/com/example/storerequisition/Adapter/RecyclerViewItemAdapter.java
+++ app/src/main/java/com/example/storerequisition/Adapter/RecyclerViewItemAdapter.java
@@ -8,21 +8,25 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.example.storerequisition.Model.ItemModel;
 import com.example.storerequisition.R;
 import com.example.storerequisition.Utils.RecyclerViewTouchListener;
 import java.util.ArrayList;
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RecyclerViewItemAdapter extends RecyclerView.Adapter<RecyclerViewItemAdapter.ViewHolder>{
     private ArrayList<ItemModel> ItemModels;
     private Context context;
     private RecyclerView recyclerView;
     private BottomSheetBehavior bottomSheetBehavior;
-    private NestedScrollView bottomSheetLayout;
+    private LinearLayout bottomSheetLayout;
+    private int lastPosition = -1;
 
-    public RecyclerViewItemAdapter(ArrayList<ItemModel> itemModels, Context context, RecyclerView recyclerView, BottomSheetBehavior bottomSheetBehavior,NestedScrollView bottomSheetLayout) {
+    public RecyclerViewItemAdapter(ArrayList<ItemModel> itemModels, Context context, RecyclerView recyclerView, BottomSheetBehavior bottomSheetBehavior,LinearLayout bottomSheetLayout) {
         ItemModels = itemModels;
         this.context = context;
         this.recyclerView = recyclerView;
@@ -41,6 +45,8 @@ public class RecyclerViewItemAdapter extends RecyclerView.Adapter<RecyclerViewIt
         ItemModel itemModel = ItemModels.get(position);
         holder.ItemName.setText(itemModel.getItemName());
         holder.ItemIcon.setImageResource(itemModel.getItemIcon());
+        //holder.itemView.getLayoutParams().width = /
+        setAnimation(holder.itemView, position);
         recyclerView.addOnItemTouchListener(new RecyclerViewTouchListener(context, recyclerView, new RecyclerViewTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
@@ -57,13 +63,22 @@ public class RecyclerViewItemAdapter extends RecyclerView.Adapter<RecyclerViewIt
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         TextView ItemName;
-        CircleImageView ItemIcon;
+        ImageView ItemIcon;
         public ViewHolder(View itemView) {
             super(itemView);
             ItemName = itemView.findViewById(R.id.ItemName);
             ItemIcon = itemView.findViewById(R.id.ItemIcon);
         }
     }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        if (position > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(context, R.anim.push_up_in);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
+    }
+
     public void toggleBottomSheet() {
         if (bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
             bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
--- app/src/main/java/com/example/storerequisition/MainActivity.java
+++ app/src/main/java/com/example/storerequisition/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.storerequisition;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.storerequisition.Adapter.RecyclerViewItemAdapter;
 import com.example.storerequisition.Model.ItemModel;
@@ -18,9 +22,10 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerViewItemAdapter recyclerViewItemAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private NestedScrollView bottomSheetLayout;
+    private LinearLayout bottomSheetLayout;
     private BottomSheetBehavior bottomSheetBehavior;
     private int goBack =0;
+    private CoordinatorLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,25 +37,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setAdapter() {
-        layoutManager = new LinearLayoutManager(this);
+        layoutManager = new GridLayoutManager(this,2);
         recyclerViewItemAdapter = new RecyclerViewItemAdapter(ItemModels,this,recyclerView,bottomSheetBehavior,bottomSheetLayout);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(recyclerViewItemAdapter);
     }
 
     private void setData() {
-        ItemModels.add(new ItemModel("Pen",R.drawable.pen));
-        ItemModels.add(new ItemModel("Pencil",R.drawable.pencil));
-        ItemModels.add(new ItemModel("Book",R.drawable.openbook));
-        ItemModels.add(new ItemModel("Eraser",R.drawable.eraser));
-        ItemModels.add(new ItemModel("Eraser",R.drawable.eraser));
-        ItemModels.add(new ItemModel("Eraser",R.drawable.eraser));
+        ItemModels.add(new ItemModel("Pen",R.drawable.ic_chrome_reader_mode));
+        ItemModels.add(new ItemModel("Pencil",R.drawable.ic_chrome_reader_mode));
+        ItemModels.add(new ItemModel("Book",R.drawable.ic_chrome_reader_mode));
+        ItemModels.add(new ItemModel("Eraser",R.drawable.ic_chrome_reader_mode));
+        ItemModels.add(new ItemModel("Paper",R.drawable.ic_chrome_reader_mode));
+        ItemModels.add(new ItemModel("Notebook",R.drawable.ic_chrome_reader_mode));
     }
 
     private void init() {
         ItemModels = new ArrayList<>();
         recyclerView = findViewById(R.id.StoreItemList);
         bottomSheetLayout = findViewById(R.id.bottom_sheet_layout);
+        mainLayout = findViewById(R.id.mainLayout);
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
     }
 
